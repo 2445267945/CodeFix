1448443a776d18f16161f4091fddfa816cfb2ffc
XD <15278345+xiao_pj@user.noreply.gitee.com>
2026-08-07 17:49:27 +0800

    py端链接VM上的RocketMQ 5.x版本 链接方式是gRPC端口为8081

--- CodeFix_PY/App/config.py
+++ CodeFix_PY/App/config.py
@@ -20,17 +20,20 @@ class LLMConfig:
     TIMEOUT = float(os.getenv("LLM_TIMEOUT", "60.0"))
 
 class VectorDBConfig:
-    raw_path = os.getenv("CHROMA_PATH", "./data/chroma_db")
-    CHROMA_PATH = os.path.normpath(os.path.join(PROJECT_ROOT, raw_path))
+    CHROMA_PATH = os.path.normpath(os.path.join(PROJECT_ROOT, os.getenv("CHROMA_PATH", "./data/chroma_db")))
     COLLECTION_NAME = os.getenv("COLLECTION_NAME", "alibaba_manual")
     OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "nomic-embed-text")
     # 检索返回条数
     N_RESULTS = int(os.getenv("VECTOR_N_RESULTS", "2"))
 
+class MQConfig:
+    # MQ地址
+    ROCKETMQ_NAMESRV_ADDR = os.getenv("ROCKETMQ_NAMESRV_ADDR")
+
 class Config:
     llm = LLMConfig()
     vector_db = VectorDBConfig()
-
+    mq = MQConfig()
 
 config = Config()
--- CodeFix_PY/App/infrastructure/mq/event_bus.py
+++ CodeFix_PY/App/infrastructure/mq/event_bus.py
@@ -1,32 +1,27 @@
 from rocketmq.v5.client import ClientConfiguration
 from rocketmq.v5.producer import Producer
 from rocketmq.v5.model import Message
+from rocketmq import Credentials
+from App.config import config
 
 class EventBus:
-    def __init__(self, endpoints: str, topic: str):
-        self.topic = topic
+    def __init__(self, endpoints: str):
         # 1. 配置客户端
-        config = ClientConfiguration(endpoints=endpoints)
+        credentials = Credentials("", "")  # 未开启认证时使用
+        mq_config = ClientConfiguration(endpoints=endpoints, credentials=credentials)
         # 2. 创建生产者实例
         # Producer 的构造函数需要 ClientConfiguration 和一个 topic 元组
-        self.producer = Producer(config, (topic,))
+        self.producer = Producer(mq_config)
         # 3. 启动生产者
         self.producer.startup()
 
-    def publish(self, payload: dict) -> None:
+    def publish(self, topic: str, payload: dict) -> None:
         """发送消息"""
-        # 1. 组装消息
         msg = Message()
-        msg.topic = self.topic
-        # 消息体需要是字节类型
+        msg.topic = topic
         msg.body = str(payload).encode('utf-8')
-
-        # 2. 发送消息（同步发送）
-        #    send 方法返回一个 SendReceipt 对象
         send_receipt = self.producer.send(msg)
-        print(f"消息发送成功，MessageId: {send_receipt.message_id}")
+        print(f"消息发送成功，Topic: {topic}, MessageId: {send_receipt.message_id}")
 
-event_bus = EventBus(
-    namesrv_addr=settings.ROCKETMQ_NAMESRV_ADDR,
-    topic=settings.AGENT_STATUS_TOPIC
-)
+
+event_bus = EventBus(endpoints=config.mq.ROCKETMQ_NAMESRV_ADDR)
